bbc5887a3f322af301404b2d296cfc86e0105cf9
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-12-04 08:44:51 +0800

    Update typed.js

--- js/typed.js
+++ js/typed.js
@@ -1,7 +1,7 @@
 var typed = new Typed("#element", {
     strings: ['Ahmad', 'Asai', 'Ahsyai', 'Udin'],
-    typeSpeed: 40,
-    backSpeed: 40,
+    typeSpeed: 60,
+    backSpeed: 60,
     backDelay: 300,
     startDelay: 1000,
     loop: true
